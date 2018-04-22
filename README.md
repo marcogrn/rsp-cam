e0d5ad01b335f479dc32a822e208c13d5ade6b65
Marco Guerini <marcoguerini@mail.com>
2018-04-22 16:27:54 +0200

    fix file folder

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # rsp-cam
+
+# crontab: 
+*/30 *,8-23 * * * yes | /home/pi/camera.sh > /dev/null 2>&1
--- camera.sh
+++ camera.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
 
-# declare the PATH of gdrive
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/games:/usr/games
-
-# DATE value used into file name
 DATE=$(date +"%Y-%m-%d_%H%M")
-
-# rapidstill command
 raspistill -o camera/image-$DATE.jpg -q 5
-
-# upload image in gdrive
 drive push camera/image-*
-
-# remove image file
 rm camera/image-*
-
-# crontab command: */30 *,8-23 * * * yes | /home/pi/camera.sh > /dev/null 2>&1
