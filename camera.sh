c5f408372773cd770523eef43d33f685ed76d665
Marco Guerini <marcoguerini@mail.com>
2018-04-23 20:12:04 +0200

    doc comments

--- camera.sh
+++ camera.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 
-PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/games:/usr/games
+# define the path where is installed drive
+# es. PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+# You don't know it? use this command: echo $PATH 
+PATH=INSERT_YOUR_PATH
+
+# every file need unique name
+# so I use date and time value
 DATE=$(date +"%Y-%m-%d_%H%M")
+
+# take a shot!
+# es. raspistill -o directory/filename.jpg -q 5
+# application -output directory/file_name -quality 5  
+# full documentation here: https://www.raspberrypi.org/app/uploads/2013/07/RaspiCam-Documentation.pdf
 raspistill -o camera/image-$DATE.jpg -q 5
+
+# come to daddy
+# uploads all the files whose name begins with image-
 drive push camera/image-*
+
+# save memory
+# remove the files whose name begins with image-
 rm camera/image-*
+
+# so long and thanks for all the fish
